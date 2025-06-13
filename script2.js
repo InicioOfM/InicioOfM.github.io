47bdba40e7a6e3550e1f295b902e68f7a1d1b3b3
InicioOfM <inicioof@gmail.com>
2025-06-13 05:33:22 -0500

    ALBUMV13

---
+++ script2.js
@@ -0,0 +1,41 @@
+let currentPage = 1;
+    const totalPages = 12;
+
+function openAlbum() {
+  const cover = document.getElementById('cover');
+  const navButtons = document.getElementById('navButtons');
+  
+  cover.style.transform = 'rotateY(-90deg)';
+  cover.style.opacity = '0';
+  
+  setTimeout(() => {
+    cover.style.display = 'none';
+    navButtons.classList.remove('hidden'); 
+  }, 1000);
+}
+
+
+    function closeAlbum() {
+      const cover = document.getElementById('cover');
+      cover.style.display = 'flex';
+      setTimeout(() => {
+        cover.style.opacity = '1';
+        cover.style.transform = 'rotateY(0deg)';
+      }, 10);
+      document.getElementById(`page${currentPage}`).classList.remove('active');
+      currentPage = 1;
+      document.getElementById(`page${currentPage}`).classList.add('active');
+    }
+
+    function changePage(direction) {
+      document.getElementById(`page${currentPage}`).classList.remove('active');
+      currentPage += direction;
+      if (currentPage < 1) currentPage = totalPages;
+      if (currentPage > totalPages) currentPage = 1;
+      document.getElementById(`page${currentPage}`).classList.add('active');
+    }
+
+
+    setTimeout(function() {
+  alert("Con mucho amor para ti mi niña ❤️✨");
+}, 1500);
